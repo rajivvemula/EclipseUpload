eaa515d7f3ed2ca9402ef5973d471a1b011deb5f
vumma <vumma@192.168.0.18>
2020-03-20 17:17:15 -0400

    Commit 1

---
+++ com.samplemaven/src/test/java/com/samplemaven/SelDemo.java
@@ -0,0 +1,11 @@
+package com.samplemaven;
+import org.testng.annotations.Test;
+
+public class SelDemo {
+
+	@Test
+	public void PrintSel()
+	{
+		System.out.println("This is Demp of Maven");
+	}
+}
